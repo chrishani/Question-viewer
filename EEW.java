4e5c777dbbe1f65d4c6619b780c39b13459ac345
chrishani <38728804+chrishani@users.noreply.github.com>
2019-01-15 18:54:56 +0530

    src submit

---
+++ EEW.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eew;
+
+
+
+/**
+ *
+ * @author dell
+ */
+public class EEW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FirstPage f = new FirstPage();
+        f.setVisible(true);
+        
+        
+    }
+    
+}
